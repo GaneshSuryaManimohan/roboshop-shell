85e5917560079cf1a4dc92096ddeb8e889ad221c
manimohan <manimohan.t22@gmail.com>
2026-06-13 18:08:27 +0530

    create ec2.sh

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,6 +2,9 @@
 
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 
+domain_name="surya-devops.online"
+hosted_zone_id="Z0198617Y0ILC4355WO7"
+
 for name in ${instances[@]}; do
     if [ $name == "shipping" ] || [ $name == "mysql" ]
     then
@@ -10,4 +13,44 @@ for name in ${instances[@]}; do
         instance_type="t3.micro"
     fi
     echo "creating instance for: $name with instance type: $instance_type"
+
+    instance_id=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type $instance_type --security-group-ids sg-0bbdd2b154434fbfd --subnet-id subnet-0272da41f92b04b12 --query 'Instances[0].InstanceId' --output text)
+
+    aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
+
+    if [ $name == "web" ]
+    then
+        aws ec2 wait instance-running --instance-ids $instance_id
+        public_ip=$(aws describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PublicIpAddress]' --output text)
+        ip_to_use=$public_ip
+    else
+        private_ip=$(aws describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text)
+        ip_to_use=$private_ip
+    fi
+
+    aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type $instance_type --security-group-ids sg-0bbdd2b154434fbfd --subnet-id subnet-0272da41f92b04b12 --query 'Instances[0].InstanceId' --output text
+
+aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch '
+{
+    "Comment": "Testing creating a record set",
+    "Changes": [
+        {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "$name.$domain_name",
+                "Type": "A",
+                "TTL": 60,
+                "ResourceRecords": [
+                    {
+                        "Value": "${ip_to_use}"
+                    }
+                ]
+            }
+        }
+    ]
+}'
+
 done
+
+
+
